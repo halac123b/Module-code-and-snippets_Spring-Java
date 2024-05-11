90ed939a9c6a3233db7e2a02eec8ac0ad9abda1a
halac123b <halac123b@gmail.com>
2024-05-12 03:05:28 +0700

    Init first project

---
+++ src/test/java/duyha/learn/spring_io/SpringIoApplicationTests.java
@@ -0,0 +1,13 @@
+package duyha.learn.spring_io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
